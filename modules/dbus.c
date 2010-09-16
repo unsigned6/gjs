f826423e7d0b013be55183d8d22e71746b2192c8
Colin Walters <walters@verbum.org>
2010-09-16 19:40:11 -0400

    xulrunner 1.9.3: Fix assumption that jsid == jsval
    The two have always been conceptually distinct types.  Even in 1.9.3,
    they are still the same in implementation, but to avoid a pile of
    warnings, we should avoid confusing them. Unfortunately, even the
    SpiderMonkey docs are wrong in a few places, so gjs' code confusing
    them is understandable.
    
    Anyways, fix up places that made this assumption.

--- modules/dbus.c
+++ modules/dbus.c
@@ -1412,7 +1412,7 @@ gjs_js_dbus_watch_name(JSContext  *context,
 static JSBool
 unique_name_getter(JSContext  *context,
                    JSObject   *obj,
-                   jsval       id,
+                   jsid        id,
                    jsval      *value_p)
 {
     const char *name;
@@ -1422,7 +1422,8 @@ unique_name_getter(JSContext  *context,
     if (!get_bus_type_from_object(context, obj, &bus_type))
         return JS_FALSE;
 
-    name = gjs_string_get_ascii(id);
+    if (!gjs_get_string_id(context, id, &name))
+        return JS_FALSE;
 
     gjs_debug_jsprop(GJS_DEBUG_DBUS, "Get prop '%s' on dbus object", name);
 
@@ -1522,7 +1523,7 @@ gjs_js_dbus_start_service(JSContext  *context,
 static JSBool
 gjs_js_dbus_get_machine_id(JSContext *context,
                            JSObject  *obj,
-                           jsval      key,
+                           jsid       key,
                            jsval     *value)
 {
     char *machine_id;
